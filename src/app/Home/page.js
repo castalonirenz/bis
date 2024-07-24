95ae5a24503c63620f48a10236c46dd38c6f3d29
Renz <castalonirenz@gmail.com>
2024-07-24 22:31:45 +0800

    test

---
+++ src/app/Home/page.js
@@ -0,0 +1,15 @@
+import Button from "@/components/Button";
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <main >
+      <div className=" d-flex bg-white">
+        test
+
+        
+      </div>
+    </main>
+  );
+}
---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Button = ({ onClick, children }) => {
+    return (
+      <button onClick={onClick} className="button">
+        test
+      </button>
+    );
+  };
+  
+  export default Button;
