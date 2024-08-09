c88b0a7264ff083c7388f6377b7742b664968846
Renz <castalonirenz@gmail.com>
2024-08-09 08:24:31 +0800

    Added: Security

---
+++ src/app/Resident/Scheduling/page.js
@@ -0,0 +1,15 @@
+import Button from "@/components/Button";
+import Image from "next/image";
+
+
+export default function Scheduling() {
+  return (
+    <main >
+      <div className=" d-flex bg-white">
+        About
+
+        
+      </div>
+    </main>
+  );
+}
--- src/security/Auth.js
+++ src/security/Auth.js
@@ -2,18 +2,20 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation';
+import { useSelector } from 'react-redux';
 
 
 
 const Auth = ({ onClick, children }) => {
     const router = useRouter()
     const [userRole, setUserRole] = useState(0)
+    const user = useSelector(state => state)
 
-
+  
     useEffect(() => {
-        if(userRole == 0){
+        if(user.user.signedIn == 0){
             // alert('You are not allowed to access this page')
-            // router.push('/', { scroll: false })
+            router.push('/', { scroll: false })
         }
     })
     
